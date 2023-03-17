5ee0e5d289d99a4d37d269fa5152209b4274ccea
Anand0605 <anandvns0510@gmail.com>
2023-03-18 02:16:54 +0530

    status changed

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -9,7 +9,7 @@ function About() {
                 <div className="left-child"><img src={profile} alt="" /></div>
                 <div className="right-child">
                     <h1>About <span>Me</span></h1>
-                    <h2>Frontend Devloper</h2>
+                    <h2>Backend Develoer</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque laborum provident minima voluptatibus facere, vel asperiores. Nobis temporibus suscipit exercitationem illum sunt.</p>
                     <div className="parent-btn"><button className='btn'>Read More</button></div>
                 </div>
